a312af8d723d9e581fd1ffdd8e5430aa9f030ac2
Anna <a.karlysheva@ukma.edu.ua>
2019-09-14 22:26:08 +0300

    delete front

---
+++ src/test/java/randomer/functionality/RandomGeneratorTest.java
@@ -0,0 +1,16 @@
+package randomer.functionality;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RandomGeneratorTest {
+    @Test
+    public void generateTest() {
+//        var generator = new RandomGenerator();
+//
+//        int sum = 0;
+//        return;
+    }
+
+}
