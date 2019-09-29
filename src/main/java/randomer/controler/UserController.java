984be31df9deaca855d6d3f65a2ba4b7c1f903f6
Anna <a.karlysheva@ukma.edu.ua>
2019-09-29 19:24:35 +0300

    registration: writes to db, always ok, user: + name, surname, email

--- src/main/java/randomer/controler/UserController.java
+++ src/main/java/randomer/controler/UserController.java
@@ -1,4 +1,43 @@
 package randomer.controler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import randomer.model.User;
+import randomer.service.UserService;
+
+@RestController
 public class UserController {
+
+    UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public MyError register (@RequestBody User user){
+        userService.save(user);
+        System.out.println(user);
+        return new MyError("ok");
+    }
+
+    private class MyError{
+        private String error;
+
+        MyError(String error) {
+            this.error = error;
+        }
+
+        public String getError() {
+            return error;
+        }
+
+        public void setError(String error) {
+            this.error = error;
+        }
+    }
 }
--- src/main/java/randomer/model/User.java
+++ src/main/java/randomer/model/User.java
@@ -1,6 +1,8 @@
 package randomer.model;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 
@@ -12,13 +14,23 @@ public class User {
     private String username;
     @NotNull
     private String password;
+    @NotNull
+    private String name;
+    @NotNull
+    private String surname;
+    @NotNull
+    @Email
+    private String email;
 
     public User() {
     }
 
-    public User(@NotNull String username, @NotNull String password) {
+    public User(@NotNull String username, @NotNull String password, @NotNull String name, @NotNull String surname, @NotNull @Email String email) {
         this.username = username;
         this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
     }
 
     public String getUsername() {
@@ -35,5 +47,40 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
