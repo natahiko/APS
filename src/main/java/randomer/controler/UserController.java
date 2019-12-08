c2475d76fbc3064e490165f3229688224936e5a6
Anna <a.karlysheva@ukma.edu.ua>
2019-12-08 12:20:29 +0200

    SimpleUser is not used - needed class is ProxyUser

--- src/main/java/randomer/controler/UserController.java
+++ src/main/java/randomer/controler/UserController.java
@@ -89,39 +89,4 @@ public class UserController {
             this.error = error;
         }
     }
-
-
-    private class SimpleUser{
-        private String username;
-        private String password;
-
-        SimpleUser(String username, String password) {
-            this.username = username;
-            this.password = password;
-        }
-
-        public String getUsername() {
-            return username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        @Override
-        public String toString() {
-            return "SimpleUser{" +
-                    "username='" + username + '\'' +
-                    ", password='" + password + '\'' +
-                    '}';
-        }
-    }
 }
